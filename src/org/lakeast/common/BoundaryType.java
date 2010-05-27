7071a6a26dc4c779a88455a64e84a687a943eeb8
starffly <starffly@7992bd95-6396-5bea-9fee-00cddd8d43fe>
2010-05-27 11:51:41 +0000

    Refactor.

--- src/org/lakeast/common/BoundaryType.java
+++
@@ -1,8 +0,0 @@
-package org.lakeast.common;
-
-/**
- * @author WANG
- */
-public enum BoundaryType {
-	NONE, WRAP, BOUNCE, STICK
-}
--- src/org/lakeast/main/Task.java
+++
@@ -1,8 +0,0 @@
-package org.lakeast.main;
-
-import org.lakeast.common.InitializeException;
-import org.lakeast.common.MyLicense;
-
-public interface Task extends MyLicense {
-	public void go(String[] args) throws InitializeException;
-}
